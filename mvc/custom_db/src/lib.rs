246779a8376808ca6170d9370acbd6010552e347
parkito <artyom-karnov@yandex.ru>
2023-01-04 14:13:27 -0700

    add user cache repo

--- mvc/custom_db/src/lib.rs
+++ mvc/custom_db/src/lib.rs
@@ -1,14 +1,81 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#![feature(map_try_insert)]
+
+use std::collections::HashMap;
+use std::fmt;
+use std::fmt::{Display, Formatter};
+use std::error::Error;
+use chrono::NaiveDate;
+
+
+#[derive(Debug)]
+pub struct RepoError {
+    msg: String,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl Display for RepoError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        todo!()
     }
 }
+
+impl Error for RepoError {}
+
+pub type RepoResult<T> = Result<T, RepoError>;
+
+#[derive(Debug)]
+pub enum UserLevel {
+    CLIENT,
+    ADMIN,
+    MODER,
+}
+
+#[derive(Debug)]
+pub struct UserEntity {
+    pub id: String,
+    pub username: String,
+    pub password: String,
+    pub birth_date: NaiveDate,
+    pub level: UserLevel,
+}
+
+pub trait EntityRepo<E, K> {
+    fn save(&mut self, entity: E) -> RepoResult<K>;
+
+    fn find(&self, key: K) -> RepoResult<Option<E>>;
+
+    fn update(&self, key: K, entity: E) -> RepoResult<E>;
+
+    fn remove(&self, key: K) -> RepoResult<K>;
+
+    fn find_all(&self) -> RepoResult<Vec<E>>;
+}
+
+pub struct UserCacheRepo {
+    map: HashMap<String, UserEntity>,
+}
+
+impl EntityRepo<UserEntity, String> for UserCacheRepo {
+    fn save(&mut self, entity: UserEntity) -> RepoResult<String> {
+        match self.map.try_insert(entity.id.to_string(), entity) {
+            Err(err) => Err(RepoError { msg: format!("User with id {} already exists", err.value.id) }),
+            Ok(some) => Ok(some.id.to_string())
+        }
+    }
+
+    fn find(&self, key: String) -> RepoResult<Option<UserEntity>> {
+        todo!()
+    }
+
+    fn update(&self, key: String, entity: UserEntity) -> RepoResult<UserEntity> {
+        todo!()
+    }
+
+    fn remove(&self, key: String) -> RepoResult<String> {
+        todo!()
+    }
+
+    fn find_all(&self) -> RepoResult<Vec<UserEntity>> {
+        todo!()
+    }
+}
+
